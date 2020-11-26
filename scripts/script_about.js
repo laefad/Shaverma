29acfb34a741d48936925cec70fbf223eccb1706
laefad <FreeFlightA@yandex.ru>
2020-11-26 13:07:27 +0300

    make reviews

--- scripts/script_about.js
+++ scripts/script_about.js
@@ -8,38 +8,40 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function initSlider() {
 	let slider = document.querySelector("#images_about");
-	let slides = slider.querySelectorAll(".slide");
+	let slides = ["imgs/about/img_1.jpg", "imgs/about/img_2.jpg", "imgs/about/img_3.jpg", "imgs/about/img_4.jpg"];
 	let inputs = slider.querySelector("#selectors")
 		.querySelectorAll("input");
 	
 	inputs = [...inputs];
-	slides = [...slides];
 	
 	let i = 0;
-	let prevSlide = slides[0];
+	
+	const setBackground = img => {
+		slider.style.background = `url(${img})`;
+		slider.style.backgroundSize = "cover";
+	}
+	
+	const changeChecked = j => {
+		inputs[i].checked = false;
+		i = j % slides.length;
+		inputs[i].checked = true;
+	}
 	
 	const styleF = () => {
-		prevSlide.style.display = "none";
-		inputs[i].checked = false;
-		i = (i+1) % slides.length;
-		prevSlide = slides[i];
-		inputs[i].checked = true;
-		prevSlide.style.display = "block";
+		changeChecked(i + 1);
+		setBackground(slides[i]);
 	}
+	
+	changeChecked(0);
 	
 	let timer = setInterval( styleF, 3000);
 	
 	inputs.forEach( (input, j) => {
 		input.addEventListener("click", () => {
 			clearInterval(timer);
-			prevSlide.style.display = "none";
-			inputs[i].checked = false;
 			
-			i = j;
-			prevSlide = slides[i];
-			inputs[i].checked = true;
-			prevSlide.style.display = "block";
-			
+			changeChecked(j);
+			setBackground(slides[i]);
 			timer = setInterval( styleF, 3000);
 		});
 	});
